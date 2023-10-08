4790e262663a21e596da059a84b6c6920a079cbd
Monika kusumanchi <kusumanchimonika1407@gmail.com>
2023-10-08 15:39:11 +0000

    added boarders and removed color

--- main.py
+++ main.py
@@ -28,22 +28,43 @@ def simulate_data_processing():
 def formatting(worksheet):
     all_values = worksheet.get_all_values()
 
-# Calculate the number of filled rows and columns
+    # Calculate the number of filled rows and columns
     num_filled_rows = len(all_values)
     num_filled_columns = len(all_values[0]) if num_filled_rows > 0 else 0
     cell_range = f"A2:{chr(ord('A') + num_filled_columns - 1)}{num_filled_rows + 1}"
     header_fmt = cellFormat(
-    backgroundColor=color(1, 0.8, 1),
-    textFormat=textFormat(bold=True, foregroundColor=color(0, 0, 0)),horizontalAlignment='CENTER')
+        backgroundColor=color(1, 0.8, 1),
+        textFormat=textFormat(bold=True, foregroundColor=color(0, 0, 0)),
+        horizontalAlignment='CENTER',
+        borders=borders(
+            top=border('SOLID', color=color(0, 0, 0)),
+            bottom=border('SOLID', color=color(0, 0, 0)),
+            left=border('SOLID', color=color(0, 0, 0)),
+            right=border('SOLID', color=color(0, 0, 0))
+        )
+    )
 
-    data_fmt = cellFormat(backgroundColor=color(182/255, 215/255, 168/255),horizontalAlignment='LEFT')
+    data_fmt = cellFormat(
+        horizontalAlignment='LEFT',
+        borders=borders(
+            top=border('SOLID', color=color(0, 0, 0)),
+            bottom=border('SOLID', color=color(0, 0, 0)),
+            left=border('SOLID', color=color(0, 0, 0)),
+            right=border('SOLID', color=color(0, 0, 0))
+        )
+    )
+
     # Format the header row
     format_cell_range(worksheet, '1', header_fmt)
 
     # Format the data rows
     format_cell_range(worksheet, cell_range, data_fmt)
+
     # Freeze the header row
     set_frozen(worksheet, rows=1)
+
+    
+
     print("Successfully made changes in the Google sheet.")
 
 match = re.search(r"/spreadsheets/d/([a-zA-Z0-9-_]+)", user_input)
@@ -60,8 +81,11 @@ else:
 credentials = ServiceAccountCredentials.from_json_keyfile_name('red-studio-400805-60aea2585639.json', ['https://www.googleapis.com/auth/spreadsheets'])
 
 gc = gspread.authorize(credentials)
+try:
+    sht1 = gc.open_by_key(FILE_ID)
+except:
+    pass
 
-sht1 = gc.open_by_key(FILE_ID)
 def one_master_sheet():
     global gc
     global sht1
